838bbc2b09436a998af48fdefacfbf6a74ece915
akbarshamjijr <akbar_shamji@brown.edu>
2024-02-03 23:39:47 -0500

    using header functions

---
+++ headers/calculator.cpp
@@ -0,0 +1,26 @@
+#include "calculator.h"
+
+double multiply(double x, double y){
+
+	return x * y;
+
+}
+
+
+double add(double x, double y){
+
+	return x + y;
+
+}
+
+double subtract(double x, double y){
+
+	return x - y;
+
+}
+
+
+double divide(double x, double y){
+
+	return x / y;
+}
---
+++ headers/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "calculator.h"
+
+
+int main(int argc, char* argv[]){
+
+	std::cout << multiply(24, 42) << std::endl;
+
+
+}
